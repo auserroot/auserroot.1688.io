0e46612e24ea2f06f19995745ff5c10f72eacc4d
auserroot <fauserfzy@gmail.com>
2020-12-29 14:31:09 +0800

    main

--- js/register.js
+++ js/register.js
@@ -109,6 +109,49 @@ $('.pho_num li').click(function(){
 })
 
 //验证
-$('_h_').mousedown(function(e){ 
-   
-});
+$(function(){
+    $('.sliding > em').on('mousedown',function(ev){
+      //获取鼠标摁下的位置
+      var down_X=ev.clientX;
+      //获取em,span的宽度
+      var span_W=$('.sliding > span').width();
+      var em_X=$('.sliding > em').get(0).offsetLeft;
+      $('.sliding > em').mousemove(function(ev){
+        //获取鼠标移动的位置
+        var move_X=ev.clientX;
+        //用移动过后的位置减去鼠标摁下的位置加上p的宽度或者 em 的left 值 因为俩者是同步的 所以 减去 其中一个就好
+        var size = move_X - down_X + span_W;              
+        if(size>0){// 移动的时候判断size>0的时候执行，Math.min选取最小值，让拖动过程中不至于超出
+          size=Math.min(($('.sliding').get(0).offsetWidth - $('.sliding > em').width()),size);
+        }else{
+          //小于0的时候 size =0； 防止有无聊的用户拖回 超出边界
+          size=0;
+        }
+        $('.sliding > span').css('width', size + 'px');
+        $('.sliding > em').css('left', size + 'px');
+        //防止拖拽过程中选中文字
+        return false;
+      });
+      $(document).mouseup(function(){
+        //鼠标松开执行,判段span到达边界时候执行
+        if(($('.sliding > em').get(0).offsetLeft) >= ($('.sliding').get(0).offsetWidth - $('.sliding > em').get(0).offsetWidth)){
+          //更改div的背景
+          $('.sliding').css({'background':'#7AC23C'});
+          //去除span背景
+          $('.sliding > span').removeClass('huakuai');
+          $('.sliding > span').css({'width':'282px','height':'36px','color':'#fff'});
+          //更改标签中的内容
+          $('.sliding > span').text('验证通过');
+          //删除鼠标摁下方法防止用户 无聊往回 推拽
+          $('.sliding > em').off('mousedown');
+        }else{
+          //判断没到达边界让它再回到起点
+          //利用jquery让em 和 span 回到起始状态
+          $('.sliding > em').animate({left:'0px'},282);
+          $('.sliding > span').animate({width:'0px'},282);
+        }
+        $(document).get(0).onmousemove=null; //最后让 鼠标移动 和 鼠标松开 事件停止 
+        $(document).get(0).onmouseup=null;
+      });
+    });
+  });
