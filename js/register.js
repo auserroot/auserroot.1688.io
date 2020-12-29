49d5b63e955424b8e749685eaf6e3f1db1c99226
auserroot <fauserfzy@gmail.com>
2020-12-29 19:23:03 +0800

    main

--- js/register.js
+++ js/register.js
@@ -91,16 +91,17 @@ $('.header_bot a').click(function () {
         $(this).siblings().css({'color': '#000','border-bottom':'1px solid #eee'})
     }
   });
+// 企业  
 $('.header_bot a:nth-child(1)').click(function () {
     $('.main_txt').html('有企业营业执照（含个体工商户）的用户请注册。权益如下：做企业实名认证；作为卖家身份开店；作为买家身份采购。');
-    var t=true;
-    if(t){
-        var str = " <div> <label for='name'>联系人姓名:</label><input type='text' id='name' placeholder='请输入真实姓名'></div><div> <label for='cpname'>企业名称:</label><input type='text' id='cpname' placeholder='营业执照上的公司名称'></div><div> <label>贸易身份:</label><input type='radio'>我要销售 <input type='radio'>我要采购 <input type='radio'>两者都是</div>"
-        $('.f_p_box').after(str);
-        return t=false;    
-    }else{
-        
-    }
+    $('.f_b_form2').css('display','block');
+    $('.f_b_form1').css('display','none');
+});
+//个人
+$('.header_bot a:nth-child(2)').click(function () {
+    $('.main_txt').html('无企业营业执照的个人用户请注册个人账户。权益如下：做个人实名认证；作为买家身份采购；');
+    $('.f_b_form1').css('display','block');
+    $('.f_b_form2').css('display','none');
 });
 
 //下拉菜单（电话国际编号）////
@@ -121,45 +122,45 @@ $('.pho_num li').click(function(){
 
 //验证
 $(function(){
-    $('.sliding > em').on('mousedown',function(ev){
+    $('._h_').on('mousedown',function(ev){
       //获取鼠标摁下的位置
       var down_X=ev.clientX;
       //获取em,span的宽度
-      var span_W=$('.sliding > span').width();
-      var em_X=$('.sliding > em').get(0).offsetLeft;
-      $('.sliding > em').mousemove(function(ev){
+      var span_W=$('.huakuai').width();
+      var em_X=$('._h_').get(0).offsetLeft;
+      $('._h_').mousemove(function(ev){
         //获取鼠标移动的位置
         var move_X=ev.clientX;
         //用移动过后的位置减去鼠标摁下的位置加上p的宽度或者 em 的left 值 因为俩者是同步的 所以 减去 其中一个就好
         var size = move_X - down_X + span_W;              
         if(size>0){// 移动的时候判断size>0的时候执行，Math.min选取最小值，让拖动过程中不至于超出
-          size=Math.min(($('.sliding').get(0).offsetWidth - $('.sliding > em').width()),size);
+          size=Math.min(($('.sliding').get(0).offsetWidth - $('._h_').width()),size);
         }else{
           //小于0的时候 size =0； 防止有无聊的用户拖回 超出边界
           size=0;
         }
-        $('.sliding > span').css('width', size + 'px');
-        $('.sliding > em').css('left', size + 'px');
+        $('.huakuai').css('width', size + 'px');
+        $('._h_').css('left', size + 'px');
         //防止拖拽过程中选中文字
         return false;
       });
       $(document).mouseup(function(){
         //鼠标松开执行,判段span到达边界时候执行
-        if(($('.sliding > em').get(0).offsetLeft) >= ($('.sliding').get(0).offsetWidth - $('.sliding > em').get(0).offsetWidth)){
+        if(($('._h_').get(0).offsetLeft) >= ($('.sliding').get(0).offsetWidth - $('._h_').get(0).offsetWidth)){
           //更改div的背景
           $('.sliding').css({'background':'#7AC23C'});
           //去除span背景
-          $('.sliding > span').removeClass('huakuai');
-          $('.sliding > span').css({'width':'282px','height':'36px','color':'#fff'});
+          $('.huakuai').removeClass('huakuai');
+          $('.huakuai').css({'width':'282px','height':'36px','color':'#fff'});
           //更改标签中的内容
-          $('.sliding > span').text('验证通过');
+          $('.huakuai').text('验证通过');
           //删除鼠标摁下方法防止用户 无聊往回 推拽
-          $('.sliding > em').off('mousedown');
+          $('._h_').off('mousedown');
         }else{
           //判断没到达边界让它再回到起点
           //利用jquery让em 和 span 回到起始状态
-          $('.sliding > em').animate({left:'0px'},282);
-          $('.sliding > span').animate({width:'0px'},282);
+          $('._h_').animate({left:'0px'},282);
+          $('.huakuai').animate({width:'0px'},282);
         }
         $(document).get(0).onmouseup=null;
       });
