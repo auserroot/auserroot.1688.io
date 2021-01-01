a07c32741c378aaeabcc7a4e27da30af32d9b5c4
auserroot <fauserfzy@gmail.com>
2021-01-01 21:07:51 +0800

    change index.js

--- js/index.js
+++ js/index.js
@@ -119,7 +119,7 @@ $('.box_list').mouseover(function(){
                 <p><span>￥ ${item.pric}</span><span>${item.offerdeal}</span></p>
                 <a href="#" class="b_l_t_a">${item.title}</a>
             </div>
-            `).appendTo(`<div class="top_l clearfix"></div>`)).appendTo($('.top_l'));
+            `).appendTo(`<div class="top_l clearfix"></div>`)).appendTo($('.box_list'));
             });
         }
     });
